7cbd39c1be8b6cbeb9818463665b0665770507c2
Joshua Chen <jzc0289@auburn.edu>
2026-08-25 18:20:09 -0500

    feat: add light status and toggle endpoints

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi import HTTPException
+from kasa import SmartPlug
+
+import os
+import uvicorn
+
+
+load_dotenv()
+app = FastAPI()
+
+_IP = os.getenv("IP")
+
+@app.get("/lights")
+async def Lights():
+    try:
+        plug = SmartPlug(_IP)
+        await plug.update()
+        status = plug.is_on
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    return {"status": status}
+
+@app.get("/lights/toggle")
+async def ToggleLights():
+    try:
+        plug = SmartPlug(_IP)
+        await plug.update()
+        if plug.is_on:
+            await plug.turn_off()
+        else:
+            await plug.turn_on()
+
+        await plug.update()
+        status = plug.is_on
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"status": status}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=3003)
