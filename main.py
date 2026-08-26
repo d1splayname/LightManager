1c2e3c36adb9b44e90688bfad7e874b947f25a60
Joshua Chen <jzc0289@auburn.edu>
2026-08-25 22:13:27 -0500

    feat: add on and off switch

--- main.py
+++ main.py
@@ -40,5 +40,31 @@ async def ToggleLights():
 
     return {"status": status}
 
+@app.get("/lights/on")  
+async def TurnOnLights():
+    try:
+        plug = SmartPlug(_IP)
+        await plug.update()
+        await plug.turn_on()
+        await plug.update()
+        status = plug.is_on
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"is_on": status}
+
+@app.get("/lights/off")
+async def TurnOffLights():
+    try:
+        plug = SmartPlug(_IP)
+        await plug.update()
+        await plug.turn_off()
+        await plug.update()
+        status = plug.is_on
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"is_on": status}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=3003)
